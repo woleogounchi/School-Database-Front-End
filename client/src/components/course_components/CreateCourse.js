2c7a012ec33856bbbb924e78c51e206cb9a01a01
Kolawole Ogounchi <kolawoleogounchi@outlook.com>
2020-02-17 10:01:09 -0500

    Update CreateCourse.js

--- client/src/components/course_components/CreateCourse.js
+++ client/src/components/course_components/CreateCourse.js
@@ -39,13 +39,13 @@ class createCourse extends Component {
       materialsNeeded,
       userId
     };
-    //In case there is no title and/or description an error is displayed
+    // If there is no title and/or description an error is displayed
     if (title === null && description === null) {
       this.setState({
         errors: ['Please add missing title and/or description']
       });
     } else {
-      //In case all the course data are correct, ensure user is authenticated and create the course
+      // If all the course data are correct, ensure user is authenticated and course created
       context.data
         .createCourse(course, emailAddress, password)
         .then(errors => {
@@ -61,6 +61,102 @@ class createCourse extends Component {
         });
     }
   };
+
+  // If course creation cancelled, user redirected to home page
+  cancel = () => {
+    this.props.history.push('/');
+  };
+
+  render() {
+    const {
+      title,
+      description,
+      estimatedTime,
+      materialsNeeded,
+      errors
+    } = this.state;
+    const user = this.props.context.authenticatedUser;
+
+    return(
+      <div className='bounds course--detail'>
+        <h1>Create Course</h1>
+        <Form
+          cancel={this.cancel}
+          errors={errors}
+          submit={this.submit}
+          submitButtonText='Create Course'
+          elements={() => (
+            <React.Fragment>
+              <div className='grid-66'>
+                <div className='course--header'>
+                  <h4 className='course--label'>Course</h4>
+                  <div>
+                    <input
+                      id='title'
+                      name='title'
+                      type='text'
+                      className='input-title course--title--input'
+                      placeholder='Course title...'
+                      value={title}
+                      onChange={this.change}
+                    />
+                  </div>
+                  <p>
+                    By {user.firstName} {user.lastName}
+                  </p>
+                </div>
+                <div className='course--description'>
+                  <div>
+                    <textarea
+                      id='description'
+                      name='description'
+                      className=''
+                      placeholder='Course description...'
+                      value={description}
+                      onChange={this.change}
+                    ></textarea>
+                  </div>
+                </div>
+              </div>
+              <div className='grid-25 grid-right'>
+                <div className='course--stats'>
+                  <ul className='course--stats--list'>
+                    <li className='course--stats--list--item'>
+                      <h4>Estimated Time</h4>
+                      <div>
+                        <input
+                          id='estimatedTime'
+                          name='estimatedTime'
+                          type='text'
+                          className='course--time--input'
+                          placeholder='Hours'
+                          value={estimatedTime}
+                          onChange={this.change}
+                        />
+                      </div>
+                    </li>
+                    <li className='course--stats--list--item'>
+                      <h4>Materials Needed</h4>
+                      <div>
+                        <textarea
+                          id='materialsNeeded'
+                          name='materialsNeeded'
+                          className=''
+                          placeholder='List materials...'
+                          value={materialsNeeded}
+                          onChange={this.change}
+                        ></textarea>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </React.Fragment>
+          )}
+        />
+      </div>
+    );
+  }
 }
 
 export default createCourse;
